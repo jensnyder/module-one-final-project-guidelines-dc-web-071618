2a359afe63c0875fb4a58ed281ba74e47e5838ff
github username <github email address>
2018-08-01 17:49:31 -0400

    Added trivia and show trivia (maximums)

--- lib/models/cli.rb
+++ lib/models/cli.rb
@@ -5,82 +5,101 @@ class CommandLineInterface
     puts "Welcome to NFL Stats Finder."
   end
 
-  #follows greeting (gets division, team, or trivia)
-  def gets_user_input
-    puts "Are you interested in a division (1) or a team (2) or trivia (3)?"
+  #general get user input method
+  def gets_user_input(string)
+    puts string
     gets.chomp
   end
 
-  #follows (1) selection for divison
-  def gets_user_division
-    puts "Enter the division (AFC or NFC)"
-    gets.chomp
-  end
-
-  #follows (2) selection for team
-  def gets_user_team
-    puts "Enter a team name"
-    gets.chomp
-  end
-
-  #follows enter team name
-  def gets_team_option
-    puts "Are you interested in players (1) or stats (2)?"
-    gets.chomp
-  end
-
-  #follows gets_user_division --> returns teams in said division
+  #(1) divisions
   def show_teams_from_division_input
-    u_division = gets_user_division
+    u_division = gets_user_input("Enter the division (AFC or NFC)")
     r_division = find_division(u_division)
     r_teams = find_teams_by_divison(r_division)
     show_teams(r_teams)
   end
 
-  #follows gets_team_option --> returns players on said team
-  def show_players_from_team_input(selection)
-    r_team = find_team(selection)
-    r_players = find_players(r_team)
-    show_players(r_players)
+  #(1) divisions and (2)team
+  def show_players_or_stats_from_team_name
+    team_name = gets_user_input("Enter a team name")
+    player_or_stats = gets_user_input("Are you interested in players (1) or stats (2)?")
+    case player_or_stats
+    when "1"
+      team = find_team(team_name)
+      players = find_players(team)
+      show_players(players)
+    when "2"
+      show_stats_from_team_name(team_name)
+    else
+      puts "Bad input."
+      show_players_or_stats_from_team_name
+    end
   end
 
-  def run
-    greet
-    input = gets_user_input
-    #doesn't allow for faulty input
-    while input != "1" && input != "2"
-      puts "Wrong format!"
-      input = gets_user_input
-    end
-    #divisions
-    if input == "1"
-      show_teams_from_division_input
-    end
-    #teams
-    selection = gets_user_team
-    n_input = gets_team_option #(1- players or 2-stats)
-    if n_input == "1"
-      show_players_from_team_input(selection)
-    elsif n_input == "2"
-      selection_stats = find_stats_by_team(selection)
-      show_teamstats(selection_stats)
+  #(3) trivia --> stats
+  def show_stats_from_team_name(team)
+    team_instance = find_team(team)
+    teamstat = Teamstat.find_by team_id: team_instance.team_id
+    puts "The stats for this team are wins: #{teamstat.team_wins} and losses: #{teamstat.team_losses}"
+  end
+
+  def show_trivia
+    input = gets_user_input("Select a piece of trivia:\n
+      (1) team with the most wins\n
+      (2) team with the most losses\n")
+    case input
+    #(1) wins
+    when "1"
+      get_max("team_wins")
+    #(2) losses
+    when "2"
+      get_max("team_losses")
     end
   end
+
+  def get_max(string)
+    max = Teamstat.maximum(string)
+    teamstat_max = Teamstat.find_by string.to_sym => max
+    id = teamstat_max.team_id
+    team_max = Team.find(id)
+    puts team_max.name
+  end
+
+  #RUN
+  def run
+    greet
+    input = gets_user_input("Are you interested in a division (1) or a team (2) or trivia (3)?")
+    case input
+    #(1) division
+    when "1"
+      show_teams_from_division_input
+      show_players_or_stats_from_team_name
+    #(2) teams
+    when "2"
+      show_players_or_stats_from_team_name
+    #(3) stats
+    when "3"
+      # team_name = gets_user_input("Enter a team name")
+      # show_stats_from_team_name(team_name)
+      show_trivia
+    else
+      puts "Bad input."
+      run
+    end
+  end
+
+  #HELPER METHODS
 
   def find_division(division)
     Division.find_by(name: division)
-  end
-
-  def find_teams_by_divison(division)
-    Team.where("division_id = ?", division.id)
   end
 
   def find_team(team)
     Team.find_by(name: team)
   end
 
-  def find_players(team)
-    team.players
+  def find_teams_by_divison(division)
+    Team.where("division_id = ?", division.id)
   end
 
   def show_teams(teams)
@@ -90,20 +109,15 @@ class CommandLineInterface
     puts "The teams in this division are: #{team_names}"
   end
 
+  def find_players(team)
+    team.players
+  end
+
   def show_players(players)
     player_names = players.map do |player|
       player.name
     end
     puts "The players on this team are: #{player_names}"
-  end
-
-  def find_stats_by_team(team)
-    team_instance = find_team(team)
-    Teamstat.find_by team_id: team_instance.team_id
-  end
-
-  def show_teamstats(teamstat)
-    puts "The stats for this team are wins: #{teamstat.team_wins} and losses: #{teamstat.team_losses}"
   end
 
 end

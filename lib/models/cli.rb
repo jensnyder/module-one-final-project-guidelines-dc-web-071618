80a4dc3005a5213bf1f0d67f908dc6b379c05c87
github username <github email address>
2018-08-01 16:05:26 -0400

    Added find_stats_by_team and show_teamstats, finna refactor cli.rb

--- lib/models/cli.rb
+++ lib/models/cli.rb
@@ -5,21 +5,31 @@ class CommandLineInterface
     puts "Welcome to NFL Stats Finder."
   end
 
+  #follows greeting (gets division, team, or trivia)
   def gets_user_input
-    puts "Are you interested in a division (1) or a team (2)?"
+    puts "Are you interested in a division (1) or a team (2) or trivia (3)?"
     gets.chomp
   end
 
+  #follows (1) selection for divison
   def gets_user_division
     puts "Enter the division (AFC or NFC)"
     gets.chomp
   end
 
+  #follows (2) selection for team
   def gets_user_team
     puts "Enter a team name"
     gets.chomp
   end
 
+  #follows enter team name
+  def gets_team_option
+    puts "Are you interested in players (1) or stats (2)?"
+    gets.chomp
+  end
+
+  #follows gets_user_division --> returns teams in said division
   def show_teams_from_division_input
     u_division = gets_user_division
     r_division = find_division(u_division)
@@ -27,9 +37,9 @@ class CommandLineInterface
     show_teams(r_teams)
   end
 
-  def show_players_from_team_input
-    u_team = gets_user_team
-    r_team = find_team(u_team)
+  #follows gets_team_option --> returns players on said team
+  def show_players_from_team_input(selection)
+    r_team = find_team(selection)
     r_players = find_players(r_team)
     show_players(r_players)
   end
@@ -47,7 +57,14 @@ class CommandLineInterface
       show_teams_from_division_input
     end
     #teams
-    show_players_from_team_input
+    selection = gets_user_team
+    n_input = gets_team_option #(1- players or 2-stats)
+    if n_input == "1"
+      show_players_from_team_input(selection)
+    elsif n_input == "2"
+      selection_stats = find_stats_by_team(selection)
+      show_teamstats(selection_stats)
+    end
   end
 
   def find_division(division)
@@ -78,6 +95,15 @@ class CommandLineInterface
       player.name
     end
     puts "The players on this team are: #{player_names}"
+  end
+
+  def find_stats_by_team(team)
+    team_instance = find_team(team)
+    Teamstat.find_by team_id: team_instance.team_id
+  end
+
+  def show_teamstats(teamstat)
+    puts "The stats for this team are wins: #{teamstat.team_wins} and losses: #{teamstat.team_losses}"
   end
 
 end
